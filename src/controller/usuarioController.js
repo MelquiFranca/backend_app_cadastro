647ffb983fd6862a5192c702b1dd698e06edd78c
Melquisedeque P. França <melquifranca@hotmail.com>
2021-06-16 19:32:08 -0300

    estrutura - criacao de controller e service de Usuarios

---
+++ src/controller/usuarioController.js
@@ -0,0 +1,19 @@
+class UsuarioController {    
+    static listar(request, response) {
+
+    }
+    static selecionar(request, response) {
+
+    }
+    static salvar(request, response) {
+        
+    }
+    static alterar(request, response) {
+
+    }
+    static excluir(request, response) {
+        
+    }
+}
+
+export default UsuarioController
