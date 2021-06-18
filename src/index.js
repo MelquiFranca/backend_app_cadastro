c8fe928880482141e3706ec131e4f3c77776274c
Melquisedeque P. França <melquifranca@hotmail.com>
2021-06-18 19:49:28 -0300

    logica - upload de arquivos

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const rotas = require('./routes.js')
 
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(rotas)
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log("Servidor iniciado.")
 })
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,27 @@
 const { Router } = require('express')
+const path = require('path')
+const multer = require('multer')
 const UsuarioController = require('./controller/usuarioController.js')
 
 const rotas = Router()
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const { codigo } = req.body
+        file.originalname = `${codigo}${path.extname(file.originalname)}`
+        cb(null, "uploads/")
+    },
+    filename: (req, file, cb) => {        
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage})
+
 rotas.get('/usuarios', UsuarioController.listar)
 rotas.get('/usuarios/:id', UsuarioController.selecionar)
-rotas.post('/usuarios', UsuarioController.salvar)
-rotas.put('/usuarios/:id', UsuarioController.alterar)
+rotas.post('/usuarios', upload.single('foto'), UsuarioController.salvar)
+rotas.put('/usuarios/:id', upload.single('foto'), UsuarioController.alterar)
 rotas.delete('/usuarios/:id', UsuarioController.excluir)
 
 module.exports = rotas
